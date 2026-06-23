4b80b0d1aaa9c79f9648d17be08ba83c9011b3fe
farena <farena@captainC.farena.com>
2026-06-24 01:29:12 +0600

    Update game project

--- Levels/02_Nazanaza/03_03.gd
+++ Levels/02_Nazanaza/03_03.gd
@@ -5,4 +5,11 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		TransitionScreen.transition()
 		await TransitionScreen.on_transition_finished
-		get_tree().change_scene_to_file("res://Levels/01_Dapa_Kingdom/02_04.tscn")
+		get_tree().change_scene_to_file("res://Levels/02_Nazanaza/03_04.tscn")
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body is Player:
+		TransitionScreen.transition()
+		await TransitionScreen.on_transition_finished
+		get_tree().reload_current_scene()
---
+++ Levels/05_Arshinagar/06_01.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		TransitionScreen.transition()
+		await TransitionScreen.on_transition_finished
+		get_tree().reload_current_scene()
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body is Player:
+		TransitionScreen.transition()
+		await TransitionScreen.on_transition_finished
+		get_tree().change_scene_to_file("res://Levels/05_Arshinagar/06_02.tscn")
