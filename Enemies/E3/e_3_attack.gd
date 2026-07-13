14254845a443aa0d16cb2c80f90f31dd4ca24a82
farena <farena@captainC.farena.com>
2026-07-14 00:45:57 +0600

    Update game project

---
+++ Enemies/E3/e_3_attack.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+func _ready() -> void:
+	await get_tree().create_timer(0.2).timeout
+	queue_free()
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		PlayerHealth.health-=20
+		body.take_damage()
