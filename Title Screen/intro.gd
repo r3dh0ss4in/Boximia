f149c11624b5b0c8d2646f051af2325470392e51
farena <farena@captainC.farena.com>
2026-07-23 23:58:23 +0600

    Update game project

--- Title Screen/intro.gd
+++ Title Screen/intro.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	Items.visible=false
 	PlayerHealth.visible=false
 	animated_sprite_2d.play("Idle")
-	await get_tree().create_timer(2).timeout
+	await get_tree().create_timer(4).timeout
 	TransitionScreen.transition()
 	await TransitionScreen.on_transition_finished
 	get_tree().change_scene_to_file("res://Title Screen/tilte.tscn")
